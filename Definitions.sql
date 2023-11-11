064ad30038a0e8c62b7d93291f7b9de48b9d9d88
Adam <33016300+adaneslavo@users.noreply.github.com>
2023-11-11 20:12:15 +0100

    Update Definitions.sql

--- Definitions.sql
+++ Definitions.sql
@@ -1,4 +1,21 @@
 --==========================================================================================================================
+-- COMPATIBILITY
+--==========================================================================================================================	
+/*
+Custom Civilizations compatibility patches!
+0 = Disabled disregarding if its detects new custom civilizations.
+1 = Enabled if it detects new custom civilizations.
+2 = Disabled until it detects something! (Default)
+*/
+
+INSERT INTO COMMUNITY	
+		(Type,								Value)
+VALUES	('ACN-CIVILIZATION-AINU', 		2), -- @gwennog's part
+		('ACN-CIVILIZATION-CORSICA', 		2);
+
+UPDATE Community SET Value = '1' WHERE Type = 'ACN-CIVILIZATION-AINU' AND EXISTS (SELECT * FROM Civilizations WHERE Type='CIVILIZATION_HININ_AINU') AND NOT EXISTS (SELECT * FROM COMMUNITY WHERE Type='ACN-CIVILIZATION-AINU' AND Value=0);
+UPDATE Community SET Value = '1' WHERE Type = 'ACN-CIVILIZATION-CORSICA' AND EXISTS (SELECT * FROM Civilizations WHERE Type='CIVILIZATION_DMS_CORSICA') AND NOT EXISTS (SELECT * FROM COMMUNITY WHERE Type='ACN-CIVILIZATION-CORSICA' AND Value=0);
+--==========================================================================================================================
 -- ATLASES
 --==========================================================================================================================	
 INSERT INTO IconTextureAtlases 
